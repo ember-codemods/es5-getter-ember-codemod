57902c7261951f0072b9db66e1b3589f78e842e0
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-22 11:34:00 +0100

    Add test for `getProperties()` on nested objects

--- __testfixtures__/es5-getter-ember-codemod/this-dot-getProperties.input.js
+++ __testfixtures__/es5-getter-ember-codemod/this-dot-getProperties.input.js
@@ -1,3 +1,5 @@
 let { foo, bar, baz } = this.getProperties('foo', 'bar', 'baz');
 
+let { foo, bar, baz } = this.nested.object.getProperties('foo', 'bar', 'baz');
+
 let { foo, barBaz } = this.getProperties('foo', 'bar.baz');
--- __testfixtures__/es5-getter-ember-codemod/this-dot-getProperties.output.js
+++ __testfixtures__/es5-getter-ember-codemod/this-dot-getProperties.output.js
@@ -1,3 +1,5 @@
 let { foo, bar, baz } = this;
 
+let { foo, bar, baz } = this.nested.object;
+
 let { foo, barBaz } = this.getProperties('foo', 'bar.baz');
