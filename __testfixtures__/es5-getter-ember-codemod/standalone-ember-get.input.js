956346c59091dd0a31ade3dfb7bade4b55aec5ae
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-22 12:03:50 +0100

    Do not transform into invalid identifiers

--- __testfixtures__/es5-getter-ember-codemod/standalone-ember-get.input.js
+++ __testfixtures__/es5-getter-ember-codemod/standalone-ember-get.input.js
@@ -3,9 +3,11 @@ import { get } from '@ember/object'
 
 let foo = get(this, 'foo');
 let foo = get(this, 'foo.bar');
+let foo = get(this, 'foo-bar');
 
 let foo = Ember.get(this, 'foo');
 let foo = Ember.get(this, 'foo.bar');
+let foo = Ember.get(this, 'foo-bar');
 
 let obj = { bar: 'baz' };
 let bar = get(obj, 'bar');
